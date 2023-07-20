3aae043dc2cc699af7c0845065c0c6b0e9d097ca
krys-tsuda <krys.tsuda@gmail.com>
2023-07-19 19:13:26 -0700

    In the directory Basic_JavaScript_Projects I created index.html and main.js. I utilized window.alert() and document.write() methods, defined two variables and concatenated them together, wrote an expression, and added comments explaining my code. I've linked index.html with main.js.

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,12 @@
+var A = "\"Hello, world!\"";      //Defining variable and giving it a string value.
+
+window.alert(A)     //Exacuting window.alert and providing it with the variable A to be displayed.
+
+var Sent1 = "This is the beginning of the string"       //Defining a variable with the beginning of a string.
+var Sent2 = " and this is the end of the string"        //Defining a variable with the end of a string.
+
+document.write(Sent1        //Exacuting document.write and concatenating a string using two variables for display.
+    + Sent2) 
+
+document.write(7+3)     // Exacuting document.write where the value of an expression will be displayed.
+
