eaff98baa3a9608686875e3cf3ec37a01c1f0de4
krys-tsuda <krys.tsuda@gmail.com>
2023-07-21 02:36:55 -0700

    In directory Project2_functions index.html and main.js were created displaying JavaScript method doccument.getElementId.

---
+++ Basic_JavaScript_Projects/HTML_event/JS/event.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn');
+    //Defining a variable that cannot be reassigned.
+btn.addEventListener("click", function onClick(event) {
+    document.body.style.backgroundColor = 'fuchsia';
+    //Changes background of body to fuchsia.
+});
