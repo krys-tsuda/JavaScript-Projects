f63317ace15432e7d0ee16d10adbf3cb679eea8f
krys-tsuda <krys.tsuda@gmail.com>
2023-08-10 01:51:38 -0700

    Updated README

--- Basic_JavaScript_Projects/HTML_event/JS/event.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById('btn');
-    //Defining a variable that cannot be reassigned.
-btn.addEventListener("click", function onClick(event) {
-    document.body.style.backgroundColor = 'fuchsia';
-    //Changes background of body to fuchsia.
-});
--- Basic_JavaScript_Projects/defer.js
+++
@@ -1,4 +0,0 @@
-function defer_Function() {
-    var x = 1;
-    document.getElementById("fun").innerHTML = (10 + x);
-}
